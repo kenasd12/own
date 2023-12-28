aea71c96ff6fcaaed51e968f2cf0b4c55282bb2d
kenasd <kenasd@example.com>
2023-12-28 13:01:35 +1100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sum } from "./sum";
+const app = express();
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+ const result = sum(2, 3);
+ res.json({ message: "Hello World", result });
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+ console.log(`Listening on port ${PORT}`);
+});
